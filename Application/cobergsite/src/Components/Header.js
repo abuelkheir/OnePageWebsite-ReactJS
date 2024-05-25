85719bc98b2276565c246164c45ed2cb41a59398
Ahmed AbuElkheir <ahmedhaboelkheir@gmail.com>
2024-05-25 15:01:30 +0300

    Added Header MobileView

--- Application/cobergsite/src/Components/Header.js
+++ Application/cobergsite/src/Components/Header.js
@@ -1,14 +1,23 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
 import logoImage from "../Assets/MainLogo/MainLogo.svg"; // Corrected path
 
-// Styled components for the header elements
 const HeaderContainer = styled.header`
   background-color: #f8f8f8;
+  width: 100%;
+  max-width: 1920px;
+  height: 369px;
   padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  box-sizing: border-box;
+  position: relative;
+
+  @media (max-width: 1080px) {
+    height: 216px;
+    max-width: 1080px;
+  }
 `;
 
 const TopBar = styled.div`
@@ -17,22 +26,43 @@ const TopBar = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-bottom: 20px;
+
+  @media (max-width: 1080px) {
+    margin-bottom: 0;
+  }
 `;
 
 const Logo = styled.div`
   font-size: 1.5rem;
   font-weight: bold;
   color: black;
+
+  @media (max-width: 1080px) {
+    img {
+      height: 133px;
+      width: 133px;
+    }
+  }
 `;
 
 const LogoImage = styled.img`
-  height: 111px; /* Adjust height as needed */
+  height: 111px;
   width: 111px;
 `;
 
 const RightBar = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const JoinUsPartnerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+
+  @media (max-width: 1080px) {
+    display: none;
+  }
 `;
 
 const JoinUs = styled.div`
@@ -50,7 +80,14 @@ const Partner = styled.div`
 `;
 
 const Navigation = styled.nav`
+  position: absolute;
+  bottom: 60px;
+  width: 100%;
   text-align: center;
+
+  @media (max-width: 1080px) {
+    display: none;
+  }
 `;
 
 const NavigationList = styled.ul`
@@ -67,9 +104,85 @@ const NavigationItem = styled.li`
   margin: 0 80px;
   font-weight: 400;
   font-style: normal;
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const HamburgerMenu = styled.div`
+  display: none;
+  cursor: pointer;
+
+  @media (max-width: 1080px) {
+    display: block;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    font-size: 66px;
+    line-height: 66px;
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+
+const MobileNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 60px;
+  right: 20px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
+  animation: ${slideIn} 0.3s ease-out;
+
+  @media (max-width: 1080px) {
+    width: 80%;
+    right: 10%;
+    top: 80px;
+  }
+`;
+
+const MobileNavItem = styled.a`
+  font-size: 1rem;
+  color: black;
+  margin: 10px 0;
+  padding: 10px 0;
+  width: 100%;
+  text-align: center;
+  font-weight: 400;
+  font-style: normal;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+    background-color: #f0f0f0;
+  }
 `;
 
 const Header = () => {
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavOpen(!isMobileNavOpen);
+  };
+
   return (
     <HeaderContainer>
       <TopBar>
@@ -77,19 +190,41 @@ const Header = () => {
           <LogoImage src={logoImage} alt="Logo" />
         </Logo>
         <RightBar>
-          <Partner>Partner with LAND Interiors</Partner>
-          <JoinUs>Join as Design Expert</JoinUs>
+          <JoinUsPartnerContainer>
+            <Partner>Partner with LAND Interiors</Partner>
+            <JoinUs>Join as Design Expert</JoinUs>
+          </JoinUsPartnerContainer>
         </RightBar>
       </TopBar>
       <Navigation>
         <NavigationList>
-          <NavigationItem>About</NavigationItem>
-          <NavigationItem>Projects</NavigationItem>
-          <NavigationItem>Studio</NavigationItem>
-          <NavigationItem>Blog</NavigationItem>
-          <NavigationItem>Contact</NavigationItem>
+          <NavigationItem>
+            <a href="#about">About</a>
+          </NavigationItem>
+          <NavigationItem>
+            <a href="#projects">Projects</a>
+          </NavigationItem>
+          <NavigationItem>
+            <a href="#studio">Studio</a>
+          </NavigationItem>
+          <NavigationItem>
+            <a href="#blog">Blog</a>
+          </NavigationItem>
+          <NavigationItem>
+            <a href="#contact">Contact</a>
+          </NavigationItem>
         </NavigationList>
       </Navigation>
+      <HamburgerMenu onClick={toggleMobileNav}>☰</HamburgerMenu>
+      {isMobileNavOpen && (
+        <MobileNav>
+          <MobileNavItem href="#about">About</MobileNavItem>
+          <MobileNavItem href="#projects">Projects</MobileNavItem>
+          <MobileNavItem href="#studio">Studio</MobileNavItem>
+          <MobileNavItem href="#blog">Blog</MobileNavItem>
+          <MobileNavItem href="#contact">Contact</MobileNavItem>
+        </MobileNav>
+      )}
     </HeaderContainer>
   );
 };
